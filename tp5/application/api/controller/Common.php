3310c63019c9bb6f9e3c49d2a3331ab38b8b3242
wenjunbo_glb <634818402@qq.com>
2019-12-12 08:17:14 +0800

    common xiu gai

--- tp5/application/api/controller/Common.php
+++ tp5/application/api/controller/Common.php
@@ -14,7 +14,7 @@ use think\Validate;
 
 class Common extends Controller
 {
-  protected $_request; //处理参数
+  protected $_request; //处理参数a
   protected $_validater; //验证请求参数
   protected $_params; //复核要求的参数
   protected $_rules = array(
